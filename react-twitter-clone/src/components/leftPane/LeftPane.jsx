32e68a9a0f05553854289d4945b62dc45fbb815f
Leila Erfanirad <leila.erfanirad@gmail.com>
2024-01-15 20:22:35 +0330

    ---

--- react-twitter-clone/src/components/leftPane/LeftPane.jsx
+++ react-twitter-clone/src/components/leftPane/LeftPane.jsx
@@ -1,18 +1,20 @@
 import './leftPane.scss'
 import { NavLink } from 'react-router-dom'
+import { FaTwitter } from "react-icons/fa";
 const LeftPane = () => {
     return (
-        <div className='left-pane'>
-            <div className="container">
-                <header>Twitter</header>
-                <nav>
-                    <NavLink to='/explore'>explore</NavLink>
-                    <NavLink to='/notification'>notification</NavLink>
-                    <NavLink to='/profile'>profile</NavLink>
-                    <NavLink to='/explore'>explore</NavLink>
+        <div className='leftPane'>
+            <div className="leftPane__container">
+                <header>
+                    <FaTwitter/>
+                </header>
+                <nav >
+                    <NavLink className='leftPane__container--leftNav' to='/profile'>profile</NavLink>
+                    <NavLink className='leftPane__container--leftNav' to='/explore'>explore</NavLink>
+                    <NavLink className='leftPane__container--leftNav' to='/notification'>notification</NavLink>
                 </nav>
                 <footer>
-                    <button className='twitt'>tweet</button>
+                    <button className='leftPane__container--button'>tweet</button>
                 </footer>
                
             </div>
