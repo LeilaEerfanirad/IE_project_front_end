5de648e9ff77ec851c24b635fb7a8f9c481caa7e
Leila Erfanirad <leila.erfanirad@gmail.com>
2024-01-26 20:51:50 +0330

    ---

--- react-twitter-clone/src/components/edit/Edit.jsx
+++ react-twitter-clone/src/components/edit/Edit.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React , { useState } from 'react'
 
 function Edit() {
-  return (
-    <div>edit</div>
-  )
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    function handleSubmit () {
+        // e.preventDefault();
+        console.log('here')
+        fetch(
+            'http://localhost:3000/login?username=${formData.username}&password=${formData.password}'
+            , {
+            method: 'POST'
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+        
+    };
+
+    return (
+        <div className="loginContainer">
+            <h2>Login</h2>
+            <form >
+                <label htmlFor="username" ></label>
+                <input
+                    placeholder='UserName'
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleChange}
+                    required
+                />
+
+                <label htmlFor="password"></label>
+                <input
+                    placeholder='Password'
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                />
+
+                <button type="submit" onClick={handleSubmit}>Login</button>
+            </form>
+    </div>
+    );
 }
 
 export default Edit
